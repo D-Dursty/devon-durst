afaa53d31dc73878726667a5e7d59191d8fb6537
D-Dursty <devondahlia@gmail.com>
2022-12-05 22:03:39 -0800

    download button

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -5,7 +5,9 @@ function Resume() {
         <div className='resumeContainer'>
 
             <div className='resume'><img width='700px' src={require(`../utilities/projects/resume-1.png`)}></img></div>
-
+            <div className='downloadContainer'>
+                <button className='downloadBtn'><a className='download' href={require(`../utilities/projects/resume-1.png`)} target="_blank" download>Download My Resume</a></button>
+            </div>
         </div>
     )
 }

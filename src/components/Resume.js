e16b614121c60fa76784d53995d0dfe008cc7812
D-Dursty <devondahlia@gmail.com>
2022-12-06 20:55:15 -0800

    edits

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,45 @@ function Resume() {
     return (
         <div className='resumeContainer'>
 
-            <div className='resume'><img width='700px' src={require(`../utilities/projects/resume-1.png`)}></img></div>
+            <div className='frontEnd'>
+                <p className='frontTitle'>Front-end Proficiencies:</p>
+                <p className='frontOne'>
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>React</li>
+                        <li>JavaScript</li>
+                    </ul>
+                </p>
+                <p className='frontTwo'>
+                    <ul>
+                        <li>JQUery</li>
+                        <li>Progressive Web Applications</li>
+                        <li>Bootstrap</li>
+                        <li>TailWind</li>
+                    </ul>
+                </p>
+            </div>
+            <hr></hr>
+            <div className='backEnd'>
+                <p className='backTitle'>Back-end Proficiencies:</p>
+                <p className='backOne'>
+                    <ul>
+                        <li>Third-Party API's</li>
+                        <li>Node.js</li>
+                        <li>Object Oriented Programming</li>
+                        <li>Express</li>
+                    </ul>
+                </p>
+                <p className='backTwo'>
+                    <ul>
+                        <li>ORM</li>
+                        <li>MVC</li>
+                        <li>MySql, Sequelize</li>
+                    </ul>
+                </p>
+            </div>
+            <hr></hr>
             <div className='downloadContainer'>
                 <button className='downloadBtn'><a className='download' href={require(`../utilities/projects/resume-1.png`)} target="_blank" download>Download My Resume</a></button>
             </div>

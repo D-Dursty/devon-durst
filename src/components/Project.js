a51df7891d24f6d64c8136c8aaf13c8ddaffe4b8
D-Dursty <devondahlia@gmail.com>
2023-01-10 19:08:53 -0800

    styling

--- src/components/Project.js
+++ src/components/Project.js
@@ -13,13 +13,13 @@ function Project() {
 
                     <div className='infoContainer'>
                         <ul className='infoList'>
-                            <li className='title'><b>The Stay Homie </b></li>
+                            <li className='title'><b>THE STAY HOMIE</b></li>
                             <li className='subtitle'>The Ultimate Get-Out-of-Work Excuse Generator</li>
                             <br/>
                             <li><div className='description'>The Stay Homie is a tongue-in-check excuse generator built using HTML, TailWind, and JavaScript</div></li>
                             <br/>
-                            <li><a className='repo' href="https://cornetj13.github.io/stay-homie/" target="_blank">Visit Site</a></li>
-                            <li><a className='repo' href="https://github.com/cornetj13/stay-homie" target="_blank">GitHub Repository</a></li>
+                            <li><a className='repo' href="https://cornetj13.github.io/stay-homie/" target="_blank">VISIT SITE</a>
+                            <a className='repo2' href="https://github.com/cornetj13/stay-homie" target="_blank">GITHUB REPOSITORY</a></li>
                         </ul>
                     </div>
 
@@ -30,13 +30,13 @@ function Project() {
 
                     <div className='infoContainerTwo'>
                         <ul className='infoList'>
-                            <li className='title'><b>Bird Brain </b></li>
+                            <li className='title'><b>BIRD BRAIN </b></li>
                             <li className='subtitle'>A Digital Journal for Bird Nerds</li>
                             <br/>
                             <li><div className='description'>Bird Brain allows users to log birding outings and track associated bird sitings with facts, picture uploads, api search data, and geolocation. This site was built using JavaScript, Sequelize, Handlebars and BootStrap.</div></li>
                             <br/>
-                            <li><a className='repo' href="https://birdbrain2022.herokuapp.com/" target="_blank">Visit Site</a></li>
-                            <li><a className='repo' href="https://github.com/Sullisters/Bird-watcher" target="_blank">GitHub Repository</a></li>
+                            <li><a className='repo' href="https://birdbrain2022.herokuapp.com/" target="_blank">VISIT SITE</a>
+                            <a className='repo2' href="https://github.com/Sullisters/Bird-watcher" target="_blank">GITHUB REPOSITORY</a></li>
                         </ul>
 
                     <div className='imageContainerTwo'>
@@ -54,13 +54,13 @@ function Project() {
 
                     <div className='infoContainer'>
                         <ul className='infoList'>
-                            <li className='title'><b>Portfolio One </b></li>
+                            <li className='title'><b>PORTFOLIO PROJECT ONE </b></li>
                             
                             <br/>
                             <li><div className='description'>A practice in flexbox, this portfolio was my first build-from-scratch assignment. It includes a seamless media query transition, and was built using HTML, and CSS. </div></li>
                             <br/>
-                            <li><a className='repo' href="https://d-dursty.github.io/devon-durst-personal-webpage/" target="_blank">Visit Site</a></li>
-                            <li><a className='repo' href="https://github.com/D-Dursty/devon-durst-personal-webpage" target="_blank">GitHub Repository</a></li>
+                            <li><a className='repo' href="https://d-dursty.github.io/devon-durst-personal-webpage/" target="_blank">VISIT SITE</a>
+                            <a className='repo2' href="https://github.com/D-Dursty/devon-durst-personal-webpage" target="_blank">GITHUB REPOSITORY</a></li>
                         </ul>
                     </div>
 

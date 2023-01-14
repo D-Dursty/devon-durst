3f6a95b9e3aef9b6469f8ff6fb6db66c0075991d
D-Dursty <devondahlia@gmail.com>
2023-01-13 20:51:43 -0800

    typo

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ function About() {
             <div className='bioContainer'>
                 <div className='aboutMe'> 
                 <br></br>
-                <p>With a colorful background in Interior Design & Project Management, Education, as well as Food and Farming, I'm excited to be diving in to the world of Web Development</p>
+                <p>With a colorful background in Interior Design & Project Management, Education, as well as Food and Farming, I'm excited to be diving into the world of Web Development</p>
                 <p>I'm passionate about equity, and look forward to using my new found skills to promote access to resources through the internet</p>
                 <p>Coding is a neverending puzzle that I look forward to perpetually solving. </p>
                 <hr></hr>

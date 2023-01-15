3fd1a6883cfcf0ad7f90614c104db6605f813fd2
D-Dursty <devondahlia@gmail.com>
2023-01-14 19:25:38 -0800

    responsive layout continued

--- src/components/About.js
+++ src/components/About.js
@@ -1,41 +1,107 @@
 import React from 'react';
 import profPic from '../utilities/devon.JPG';
-import '../styles/about.css'
-function About() {
-    return (
-        <div className='aboutContainer'>
-            <div className='picContainer'>
-                <img className='profilePic' src={profPic} alt="Devon Durst" />
-            </div>
-            <div className='bioContainer'>
-                <div className='aboutMe'> 
-                <br></br>
-                <p>With a colorful background in Interior Design & Project Management, Education, as well as Food and Farming, I'm excited to be diving into the world of Web Development</p>
-                <p>I'm passionate about equity, and look forward to using my new found skills to promote access to resources through the internet</p>
-                <p>Coding is a neverending puzzle that I look forward to perpetually solving. </p>
-                <hr></hr>
-                <br></br>
-                <br></br>
-                <img className='logo' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" width="50" height="50"/>
-                <img className='logo' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"width="50" height="50" />
-                <img className='logo' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain-wordmark.svg" width="50" height="50"/>
-                <img className='logo' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" width="50" height="50"/>
-                <img className='logo' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" width="50" height="50"/>
-                <img className='logo' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" width="50" height="50" />
-                <img className='logo' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg" width="50" height="50" />
-                <img className='logo' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain-wordmark.svg" width="50" height="50" />
-                <img className='logo' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" width="50" height="50" />
-                <br></br>
-                <br></br>
-                <hr></hr>
-                <div className='downloadContainer'>
-                <button className='downloadBtn'><a className='download' href={require(`../utilities/projects/resume-1.png`)} target="_blank" download>DOWNLOAD MY RÉSUMÉ</a></button>
-            </div>
-                </div>
-            </div>
-        </div>
 
-    )
+import '../styles/about.css'
+
+
+function About() {
+  return (
+    <div className="aboutContainer">
+      <div className="picContainer">
+        <img className="profilePic" src={profPic} alt="Devon Durst" />
+      </div>
+      <div className="bioContainer">
+        <div className="aboutMe">
+          <br></br>
+          <p>
+            With a colorful background in Interior Design & Project Management,
+            Education, as well as Food and Farming, I'm excited to be diving
+            into the world of Web Development
+          </p>
+          <p>
+            I'm passionate about equity, and look forward to using my new found
+            skills to promote access to resources through the internet
+          </p>
+          <p>
+            Coding is a neverending puzzle that I look forward to perpetually
+            solving.{" "}
+          </p>
+          <hr></hr>
+          <br></br>
+          <br></br>
+          <img
+            className="logo"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
+            width="50"
+            height="50"
+          />
+          <img
+            className="logo"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+            width="50"
+            height="50"
+          />
+          <img
+            className="logo"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain-wordmark.svg"
+            width="50"
+            height="50"
+          />
+          <img
+            className="logo"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
+            width="50"
+            height="50"
+          />
+          <img
+            className="logo"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+            width="50"
+            height="50"
+          />
+          <img
+            className="logo"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
+            width="50"
+            height="50"
+          />
+          <img
+            className="logo"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg"
+            width="50"
+            height="50"
+          />
+          <img
+            className="logo"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain-wordmark.svg"
+            width="50"
+            height="50"
+          />
+          <img
+            className="logo"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg"
+            width="50"
+            height="50"
+          />
+          <br></br>
+          <br></br>
+          <hr></hr>
+          <div className="downloadContainer">
+            <button className="downloadBtn">
+              <a
+                className="download"
+                href={require(`../utilities/projects/resume-1.png`)}
+                target="_blank"
+                download
+              >
+                DOWNLOAD MY RÉSUMÉ
+              </a>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,67 @@
 import React from "react";
 import '../styles/navBar.css';
+
 import { Link } from "react-router-dom";
+import { LinkContainer } from 'react-router-bootstrap';
+import {Navbar, Nav} from 'react-bootstrap';
+
+// function NavBar() {
+//   return (
+//     <nav className="navBar">
+//       <div className="navContainer">
+//         <ul className="navTabs">
+//           <li className="navBtn">
+//             <Link to="/" className="link">
+//               ABOUT ME
+//             </Link>
+//           </li>
+//           <li className="navBtn">
+//             <Link to="/works" className="link">
+//               WORKS
+//             </Link>
+//           </li>
+//           <li className="navBtn">
+//             <Link to="/message" className="link">
+//               CONTACT
+//             </Link>
+//           </li>
+//         </ul>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default NavBar;
 
 function NavBar() {
-    return (
-      <nav className="navBar">
-        <div className="navContainer">
+  return (
+    <Navbar bg="light" expand="lg" className="navBar">
+      {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
+      <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="navContainer">
+        <span>
         <ul className="navTabs">
           <li className="navBtn">
-            <Link to="/" className="link">ABOUT ME</Link>
+            <LinkContainer to="/" className="link">
+             <Nav.Link><span>ABOUT</span> </Nav.Link> 
+            </LinkContainer>
           </li>
           <li className="navBtn">
-            <Link to="/works" className="link">WORKS</Link>
+            <LinkContainer to="/works" className="link">
+             <Nav.Link>WORKS</Nav.Link> 
+            </LinkContainer>
           </li>
           <li className="navBtn">
-            <Link to="/message" className="link">CONTACT</Link>
+            <LinkContainer to="/message" className="link">
+             <Nav.Link>CONTACT</Nav.Link> 
+            </LinkContainer>
           </li>
-        
         </ul>
-        </div> 
-      </nav>
+        </span>
+      </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
 
-    );
-  }
-  
-  export default NavBar;
+export default NavBar;
